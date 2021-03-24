bbd00617f3a52e461cb89a7defa5d89d6fa3e428
iamcho2 <thatgrass@naver.com>
2021-03-24 21:05:13 +0900

    [resolve #6] Constant 작성
    ### Description
    입력 안 되어있던 Storyboard ID 작성
    Const 구조체 만들고 하위에 Storyboard, VC, Xib 구조체 만들어서
    Name, Identifier과 같은 필요한 상수들을 관리하도록 했음.
    #5에서 만들어놓은 Storyboard, VC에 대한 상수들 선언

---
+++ Beer-Itda/Beer-Itda/Resources/Constants/Xib.swift
@@ -0,0 +1,22 @@
+//
+//  Xib.swift
+//  Beer-Itda
+//
+//  Created by 초이 on 2021/03/24.
+//
+
+import Foundation
+
+extension Const {
+    struct Xib {
+        
+        struct Name {
+            
+        }
+        
+        struct Identifier {
+            
+        }
+        
+    }
+}
