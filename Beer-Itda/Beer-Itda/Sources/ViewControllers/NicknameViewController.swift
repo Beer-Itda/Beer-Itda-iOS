a7eb07856fe93b42c7b90b93aadc8b63f53c85eb
iamcho2 <thatgrass@naver.com>
2021-03-29 00:46:37 +0900

    [#26] navigation bar Extension 리팩토링
    ### Description
    - navigation bar back button item 함수 수정
    - LoginMethod 뷰 init navi bar 함수 body 변경
    - Nickname 뷰 init navi bar 함수 생성
    
    ### Related Issue
    Resolve #26

--- Beer-Itda/Beer-Itda/Sources/ViewControllers/NicknameViewController.swift
+++ Beer-Itda/Beer-Itda/Sources/ViewControllers/NicknameViewController.swift
@@ -9,21 +9,18 @@ import UIKit
 
 class NicknameViewController: UIViewController {
 
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        initializeNavigationBar()
     }
+
+    // MARK: - Functions
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func initializeNavigationBar() {
+        self.navigationController?.initializeNavigationBarWithBackButton(navigationItem: self.navigationItem)
     }
-    */
 
 }
