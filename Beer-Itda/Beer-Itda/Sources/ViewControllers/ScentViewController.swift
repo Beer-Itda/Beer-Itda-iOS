eceda00dad7ac867768b4c479639ea16d466292e
iamcho2 <thatgrass@naver.com>
2021-04-18 11:10:25 +0900

    [#39] Scent View wireframe logic
    ### Description
    - IBAction, IBOutlet 연결
    - 기본 로직 구현
    
    ### Related issue
    Resolve #39

--- Beer-Itda/Beer-Itda/Sources/ViewControllers/ScentViewController.swift
+++ Beer-Itda/Beer-Itda/Sources/ViewControllers/ScentViewController.swift
@@ -8,13 +8,41 @@
 import UIKit
 
 class ScentViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    let screenWidth = UIScreen.main.bounds.width
+    
+    // MARK: - @IBOutlet Properties
 
+    @IBOutlet weak var selectedScentCollectionView: UICollectionView!
+    @IBOutlet weak var scentCollectionView: UICollectionView!
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        assignDelegate()
+        assignDataSource()
+        registerXib()
+        initializeNavigationBar()
     }
     
+    // MARK: - Functions
+    
+    
+    private func registerXib() {
+        selectedScentCollectionView.register(UINib(nibName: Const.Xib.Name.selectedFilterCollectionViewCell, bundle: nil), forCellWithReuseIdentifier: Const.Xib.Identifier.selectedFilterCollectionViewCell)
+        scentCollectionView.register(UINib(nibName: Const.Xib.Name.roundedSquareCollectionViewCell, bundle: nil), forCellWithReuseIdentifier: Const.Xib.Identifier.roundedSquareCollectionViewCell)
+    }
+    
+    private func initializeNavigationBar() {
+        self.navigationController?.initializeNavigationBarWithBackButton(navigationItem: self.navigationItem)
+    }
+    
+}
 
     /*
     // MARK: - Navigation
