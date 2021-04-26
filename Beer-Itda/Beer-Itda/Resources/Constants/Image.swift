e865e399c96584a1a46a133b5788a9354645a98b
iamcho2 <thatgrass@naver.com>
2021-04-26 13:36:22 +0900

    [#9] main view navigation bar item
    ### Description
    - init main view navigation bar item
    - 향, 스타일 뷰 연결
    - 탭바 active 탭 별 navigation bar item 분기처리
    - 스타일 뷰 연결 시 back button 분기처리
    - BeerAwardHeaderView 높이 조정
    
    ### Related Issue
    Resolve #9

---
+++ Beer-Itda/Beer-Itda/Resources/Constants/Image.swift
@@ -0,0 +1,14 @@
+//
+//  Image.swift
+//  Beer-Itda
+//
+//  Created by 초이 on 2021/04/26.
+//
+
+import UIKit
+
+extension Const {
+    struct Image {
+        static let btnFilter = UIImage(named: "btnFilter")
+    }
+}
