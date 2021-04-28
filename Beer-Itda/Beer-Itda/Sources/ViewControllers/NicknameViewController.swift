f8ccebbd8a16a450e940f2ea2774dfeb75494979
iamcho2 <thatgrass@naver.com>
2021-04-28 21:21:37 +0900

    [#15] Nickname 뷰 text field
    ### Description
    - 기본 텍스트 지정
    - clear button
    - 닉네임 정규식
    - text field design
    - 정규식 확인 후 uiview color 분기처리
    - 아무것도 입력되어 있지 않으면 버튼 비활성화
    
    ### Related Issue
    Resolve #15

--- Beer-Itda/Beer-Itda/Sources/ViewControllers/NicknameViewController.swift
+++ Beer-Itda/Beer-Itda/Sources/ViewControllers/NicknameViewController.swift
@@ -8,13 +8,21 @@
 import UIKit
 
 class NicknameViewController: UIViewController {
-
+    
+    // MARK: - @IBOutlet Properties
+    
+    @IBOutlet weak var nicknameTextField: UITextField!
+    @IBOutlet weak var underlineView: UIView!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var completeButton: UIButton!
+    
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         initializeNavigationBar()
+        initUnderLineAndLabel()
     }
     
     // MARK: - @IBAction Functions
@@ -22,11 +30,30 @@ class NicknameViewController: UIViewController {
     @IBAction func touchCompleteButton(_ sender: Any) {
         pushToStyleViewController()
     }
-
+    
+    @IBAction func editTextField(_ sender: Any) {
+        
+        if checkRegex(nickname: nicknameTextField.text ?? "") {
+            // 사용 가능한 닉네임
+            showCorrectNickname()
+            completeButton.isEnabled = true
+        } else {
+            // 형식에 어긋나는 닉네임
+            showRegexError()
+            completeButton.isEnabled = false
+        }
+    }
+    
     // MARK: - Functions
     
     private func initializeNavigationBar() {
         self.navigationController?.initializeNavigationBarWithBackButton(navigationItem: self.navigationItem)
+    }
+    
+    private func initUnderLineAndLabel() {
+        underlineView.tintColor = UIColor.gray
+        errorLabel.text = ""
+        completeButton.isEnabled = true
     }
     
     private func pushToStyleViewController() {
@@ -36,6 +63,37 @@ class NicknameViewController: UIViewController {
         }
         styleViewController.styleViewUsage = .onboarding
         self.navigationController?.pushViewController(styleViewController, animated: true)
+    }
+    
+    private func checkRegex(nickname: String) -> Bool {
+        
+        let pattern = "^([a-zA-Z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣]).{1,10}$"
+        let regex = try? NSRegularExpression(pattern: pattern)
+        
+        if let _ = regex?.firstMatch(in: nickname, options: [], range: NSRange(location: 0, length: nickname.count)) {
+            
+            // 닉네임이 형식에 맞을 때
+            return true
+        }
+        return false
+    }
+    
+    private func showRegexError() {
+        underlineView.backgroundColor = UIColor.red
+        errorLabel.textColor = UIColor.red
+        errorLabel.text = "사용할 수 없는 닉네임입니다."
+    }
+    
+    private func showCorrectNickname() {
+        underlineView.backgroundColor = UIColor.green
+        errorLabel.textColor = UIColor.green
+        errorLabel.text = "사용 가능한 닉네임입니다."
+    }
+    
+    private func showAlreadUsingError() {
+        underlineView.backgroundColor = UIColor.red
+        errorLabel.textColor = UIColor.red
+        errorLabel.text = "이미 존재하는 닉네임입니다."
     }
 
 }
