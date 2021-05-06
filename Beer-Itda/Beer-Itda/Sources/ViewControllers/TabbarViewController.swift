5e2d7b9963ac8ddb8f37ecec3084778ff8670386
iamcho2 <thatgrass@naver.com>
2021-05-06 18:49:30 +0900

    [#34] navigation controller 분리
    ### Description
    - navigation controller 탭별로 따로 embed 처리
    - navigation init 함수 옮김
    
    ### Related issue
    Resolve #34

--- Beer-Itda/Beer-Itda/Sources/ViewControllers/TabbarViewController.swift
+++ Beer-Itda/Beer-Itda/Sources/ViewControllers/TabbarViewController.swift
@@ -13,48 +13,8 @@ class TabbarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        initNavigationBar()
-        
-        // TODO: - tabbar 진입점이 무조건 Main인지 확인 필요
-        initFilterButton()
     }
     
     // MARK: - Functions
-    
-    private func initNavigationBar() {
-        self.navigationController?.initializeNavigationBarWithoutBackButton(navigationItem: self.navigationItem)
-    }
-    
-    @objc func touchFilterButton() {
-        let styleStoryboard = UIStoryboard(name: Const.Storyboard.Name.style, bundle: nil)
-        guard let styleViewController = styleStoryboard.instantiateViewController(withIdentifier: Const.ViewController.Identifier.style) as? StyleViewController else {
-            return
-        }
-        styleViewController.styleViewUsage = .main
-        self.navigationController?.pushViewController(styleViewController, animated: true)
-    }
-    
-    private func initFilterButton() {
-        // 필터 버튼
-        let filterButton = UIBarButtonItem(image: Const.Image.btnFilter, style: .plain, target: self, action: #selector(touchFilterButton))
-        filterButton.tintColor = UIColor.Black
 
-        self.navigationItem.leftBarButtonItem = filterButton
-    }
-
-}
-
-// MARK: - UITabBarControllerDelegate
-
-extension TabbarViewController: UITabBarControllerDelegate {
-    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        switch item.title! {
-        case "메인":
-            initFilterButton()
-        default:
-            self.navigationItem.leftBarButtonItem = nil
-        }
-    }
-    
 }
