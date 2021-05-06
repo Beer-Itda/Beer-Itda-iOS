7f71ee6cfbd8fd0385aa95412e6cccc1a344b793
iamcho2 <thatgrass@naver.com>
2021-05-06 18:53:15 +0900

    [#17] 검색하기 뷰 - 검색바 디자인
    ### Description
    - 검색 바 커스터마이징
    - tableview delegate
    - tableview datasource
    
    ### Related Issue
    Resolve #17

--- Beer-Itda/Beer-Itda/Sources/ViewControllers/SearchViewController.swift
+++ Beer-Itda/Beer-Itda/Sources/ViewControllers/SearchViewController.swift
@@ -8,22 +8,72 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    // MARK: - @IBOutlet Properties
+    
+    @IBOutlet weak var searchTableView: UITableView!
+    
+    // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        initSearchBar()
     }
     
+    // MARK: - Functions
+    
+    private func initSearchBar() {
+        // 검색 버튼
+        let search = UIBarButtonItem(systemItem: .search, primaryAction: UIAction(handler: { _ in
+            // search action
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        }))
+        self.navigationItem.rightBarButtonItem = search
+        self.navigationItem.rightBarButtonItem?.tintColor = UIColor.black
+        
+        // searchBar 만들기
+        let searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: view.frame.width - 80, height: 0))
+        
+        // placeholder
+        searchBar.placeholder = "맥주를 검색해보세요"
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: searchBar)
+        
+        // 왼쪽 돋보기 아이콘 없애기
+        searchBar.setImage(UIImage(), for: UISearchBar.Icon.search, state: .normal)
+        
+        // textfield 배경색 없애기
+        searchBar.searchTextField.backgroundColor = UIColor.clear
+        
+        // textfield 글자크기
+        searchBar.searchTextField.font = UIFont.systemFont(ofSize: 14)
     }
-    */
+    
+    private func assignDelegate() {
+        self.searchTableView.delegate = self
+    }
+    
+    private func assignDataSource() {
+        self.searchTableView.dataSource = self
+    }
+}
 
+// MARK: - UITableViewDelegate
+
+extension SearchViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - UITableViewDataSource
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
