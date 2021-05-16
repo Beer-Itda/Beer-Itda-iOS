767965d4a4c042dd68b289849d4c7483fc660ea9
iamcho2 <thatgrass@naver.com>
2021-05-16 14:10:32 +0900

    [#42] 맥주 상세 리뷰
    ### Description
     - 3줄 max
     - 더보기 버튼 분기처리
     - 더보기 버튼
     - 접기 버튼
     - 더보기 버튼 동작
     - 접기 버튼 동작
     - 컴포넌트 @IBOutlet 연결
    
    ### Related issue
    Resolve #42

--- Beer-Itda/Beer-Itda/Sources/ViewControllers/BeerDetailViewController.swift
+++ Beer-Itda/Beer-Itda/Sources/ViewControllers/BeerDetailViewController.swift
@@ -11,9 +11,15 @@ class BeerDetailViewController: UIViewController {
     
     // MARK: - Properties
     
+    // scents
     let scentView4Height: CGFloat = 32
-    
     var scents = ["시트러스 향", "시트러스 향", "시트러스 향", "시트러스 향"]
+    var scentViews = [UIView]()
+    var scentLabels = [UILabel]()
+    
+    // reviews
+    var reviewTextViews = [UITextView]()
+    var reviewMoreButtons = [UIButton]()
     
     // MARK: - @IBOutlet Properties
     
@@ -25,6 +31,7 @@ class BeerDetailViewController: UIViewController {
     @IBOutlet weak var abvLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     
+    // scents
     @IBOutlet weak var scentStackView: UIStackView!
     @IBOutlet weak var scentView1: UIView!
     @IBOutlet weak var scentLabel1: UILabel!
@@ -36,8 +43,27 @@ class BeerDetailViewController: UIViewController {
     @IBOutlet weak var scentLabel4: UILabel!
     @IBOutlet weak var scentView4HeightConstraint: NSLayoutConstraint!
     
-    var scentViews = [UIView]()
-    var scentLabels = [UILabel]()
+    // star
+    @IBOutlet weak var starStackView: UIStackView!
+    
+    // review
+    @IBOutlet weak var nicknameLabel1: UILabel!
+    @IBOutlet weak var starLabel1: UILabel!
+    @IBOutlet weak var dateLabel1: UILabel!
+    @IBOutlet weak var reviewTextView1: UITextView!
+    @IBOutlet weak var moreButton1: UIButton!
+    
+    @IBOutlet weak var nicknameLabel2: UILabel!
+    @IBOutlet weak var starLabel2: UILabel!
+    @IBOutlet weak var dateLabel2: UILabel!
+    @IBOutlet weak var reviewTextView2: UITextView!
+    @IBOutlet weak var moreButton2: UIButton!
+    
+    @IBOutlet weak var nicknameLabel3: UILabel!
+    @IBOutlet weak var starLabel3: UILabel!
+    @IBOutlet weak var dateLabel3: UILabel!
+    @IBOutlet weak var reviewTextView3: UITextView!
+    @IBOutlet weak var moreButton3: UIButton!
     
     // MARK: - View Life Cycle
 
@@ -45,6 +71,11 @@ class BeerDetailViewController: UIViewController {
         super.viewDidLoad()
 
         initScentViews()
+        initReviewTextViewsMaxLines()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        initMoreButtons()
     }
     
     // MARK: - Functions
@@ -77,6 +108,74 @@ class BeerDetailViewController: UIViewController {
         }
     }
     
+    private func initReviewTextViewsMaxLines() {
+        reviewTextViews.append(reviewTextView1)
+        reviewTextViews.append(reviewTextView2)
+        reviewTextViews.append(reviewTextView3)
+        
+        reviewMoreButtons.append(moreButton1)
+        reviewMoreButtons.append(moreButton2)
+        reviewMoreButtons.append(moreButton3)
+        
+        for idx in 0..<3 {
+            reviewTextViews[idx].textContainer.maximumNumberOfLines = 3
+            reviewTextViews[idx].textContainer.lineBreakMode = .byTruncatingTail
+        }
+    }
+    
+    private func initMoreButtons() {
+        
+        for idx in 0..<3 {
+            
+            let lineCount = (reviewTextViews[idx].contentSize.height - reviewTextViews[idx].textContainerInset.top - reviewTextViews[idx].textContainerInset.bottom) / reviewTextViews[idx].font!.lineHeight
+            
+            if lineCount <= 3 {
+                reviewMoreButtons[idx].isHidden = true
+            }
+        }
+    }
+    
     // MARK: - @IBAction Functions
 
+    @IBAction func touchMoreButton1(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        
+        if sender.isSelected {
+            // 펼치기
+            reviewTextView1.textContainer.maximumNumberOfLines = 0
+            reviewTextView1.invalidateIntrinsicContentSize()
+        } else {
+            // 접기
+            reviewTextView1.textContainer.maximumNumberOfLines = 3
+            reviewTextView1.invalidateIntrinsicContentSize()
+        }
+    }
+    
+    @IBAction func touchMoreButton2(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        
+        if sender.isSelected {
+            // 펼치기
+            reviewTextView2.textContainer.maximumNumberOfLines = 0
+            reviewTextView2.invalidateIntrinsicContentSize()
+        } else {
+            // 접기
+            reviewTextView2.textContainer.maximumNumberOfLines = 3
+            reviewTextView2.invalidateIntrinsicContentSize()
+        }
+    }
+    
+    @IBAction func touchMoreButton3(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        
+        if sender.isSelected {
+            // 펼치기
+            reviewTextView3.textContainer.maximumNumberOfLines = 0
+            reviewTextView3.invalidateIntrinsicContentSize()
+        } else {
+            // 접기
+            reviewTextView3.textContainer.maximumNumberOfLines = 3
+            reviewTextView3.invalidateIntrinsicContentSize()
+        }
+    }
 }
