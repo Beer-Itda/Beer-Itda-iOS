adf989fe07738df22a9df6a6fcd02f173a1b77d7
iamcho2 <thatgrass@naver.com>
2021-06-11 22:46:37 +0900

    [#70] 우리 집 주변 바틀샵 Cell 만들기
    ### Description
     - Cell 만들기
     - MainTableView에 연결
     - cell height 분기처리
    
    ### Related Issue
    Resolve #70

---
+++ Beer-Itda/Beer-Itda/Sources/Cells/Main/BottleShopTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  BottleShopTableViewCell.swift
+//  Beer-Itda
+//
+//  Created by 초이 on 2021/06/10.
+//
+
+import UIKit
+
+class BottleShopTableViewCell: UITableViewCell {
+    
+    // MARK: - @IBOutlet Properties
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var beerImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        initViews()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    // MARK: - Functions
+    private func initViews() {
+        // rounding yellow view
+        yellowView.makeRounded(radius: yellowView.frame.height / 2)
+        
+        // rotate beer image - 15 degree
+        beerImageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * -15 / 180.0));
+    }
+    
+}
