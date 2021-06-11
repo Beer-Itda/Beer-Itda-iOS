58a9a21d92eecaf53f9139a939a65eee5d5e73ee
iamcho2 <thatgrass@naver.com>
2021-06-12 02:27:13 +0900

    [#74] 마이페이지 뷰
    ### Description
    - 뷰 설계
    - 컴포넌트 배치
    - 뷰 연결
    - Progress bar height 조절
    
    ### Related Issue
    Resolve #74

--- Beer-Itda/Beer-Itda/Sources/ViewControllers/MyPageViewController.swift
+++ Beer-Itda/Beer-Itda/Sources/ViewControllers/MyPageViewController.swift
@@ -8,22 +8,46 @@
 import UIKit
 
 class MyPageViewController: UIViewController {
-
+    
+    // MARK: - @IBOutlet Properties
+    @IBOutlet weak var profileView: UIView!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var gradeBadgeView: UIView!
+    @IBOutlet weak var gradeGuideView: UIView!
+    
+    @IBOutlet weak var reviewBgView: UIView!
+    @IBOutlet weak var likedBeerBgView: UIView!
+    @IBOutlet weak var gradeProgressView: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        initNavigationBar()
+        initViewRounding()
+        initProgressView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Functions
+    
+    private func initNavigationBar() {
+        self.navigationController?.initializeNavigationBarWithoutBackButton(navigationItem: self.navigationItem)
+        
+        // 설정 버튼
     }
-    */
+    
+    private func initViewRounding() {
+        profileView.makeRounded(radius: profileView.frame.height / 2)
+        gradeBadgeView.makeRounded(radius: gradeBadgeView.frame.height / 2)
+        gradeGuideView.makeRounded(radius: gradeGuideView.frame.height / 2)
+        reviewBgView.makeRounded(radius: 10)
+        likedBeerBgView.makeRounded(radius: 10)
+    }
+    
+    private func initProgressView() {
+        gradeProgressView.layer.cornerRadius = 7
+        gradeProgressView.clipsToBounds = true
+        gradeProgressView.layer.sublayers![1].cornerRadius = 7
+        gradeProgressView.subviews[1].clipsToBounds = true
+    }
 
 }
